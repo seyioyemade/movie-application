4de0dcb7a5f4d4d442fab67d090c4a78af8562d1
Seyi Oyemade <seyioyemade918@gmail.com>
2023-04-19 09:22:28 +0100

    Include a popup container

--- src/index.js
+++ src/index.js
@@ -3,6 +3,9 @@ import logo from './assets/logo.png';
 
 document.querySelector('#logo').src = logo;
 
+const popupContainer = document.querySelector('.popup-cont');
+
+
 const api = 'https://api.tvmaze.com/shows'
 
 const getJSONData = async () => {
@@ -11,52 +14,64 @@ const getJSONData = async () => {
 
   display(jsonData[0].image.medium, jsonData[0].name, jsonData[0].language, jsonData[0].schedule.days[0],
     jsonData[0].schedule.time, jsonData[0].rating.average, jsonData[0].status);
-  console.log(jsonData[0])
+  console.log(jsonData[0]);
+
+  close();
 
 };
 
-const section = document.querySelector('section');
-section.className = 'popup'
-
 const display = (img, title, lang, days, time, rating, status) => {
-  // const section = document.createElement("section");
 
-const popup = ` <div><span class="material-symbols-outlined">close</span></div>
-                <div>
-                  <div class="movie-img"><img src="${img}" alt="movie-img"></div>
+  const section = document.createElement("section");
+  section.className = 'popup'
+
+  const popup = ` <div><button class="material-symbols-outlined close">close</button></div>
                   <div>
+                    <div class="movie-img"><img src="${img}" alt="movie-img"></div>
                     <div>
-                        <h1>${title}</h1>
-                        <p>Language: ${lang}</p>
-                        <p>Schedule: ${days}, ${time}</p>
-                        <p>Rating: ${rating}</p>
-                        <p>Status: ${status}</p>
+                      <div>
+                          <h1>${title}</h1>
+                          <p>Language: ${lang}</p>
+                          <p>Schedule: ${days}, ${time}</p>
+                          <p>Rating: ${rating}</p>
+                          <p>Status: ${status}</p>
+                      </div>
                     </div>
-                  </div>
-                  <div class="comments">
-                    <h2>Comments()</h2>
-                    <p>Lorem ipsum dolor sit amet consectetur.</p>
-                    <p>Lorem ipsum dolor sit amet consectetur.</p>
-                  </div>
-                  <div class="comment-form">
-                    <h2>Add a comment</h2>
-                    <form action="">
-                        <input type="text" name="name" id="name" placeholder="Your name">
-                        <textarea name="" id="" cols="30" rows="10" placeholder="Your insights"></textarea>
-                        <div class="submit-cont"><button type="submit">Comment</button></div>
-                    </form>
-                  </div>
-                    
-                </div>`;
+                    <div class="comments">
+                      <h2>Comments()</h2>
+                      <p>Lorem ipsum dolor sit amet consectetur.</p>
+                      <p>Lorem ipsum dolor sit amet consectetur.</p>
+                    </div>
+                    <div class="comment-form">
+                      <h2>Add a comment</h2>
+                      <form action="">
+                          <input type="text" name="name" id="name" placeholder="Your name">
+                          <textarea name="" id="" cols="30" rows="10" placeholder="Your insights"></textarea>
+                          <div class="submit-cont"><button type="submit">Comment</button></div>
+                      </form>
+                    </div>
+                      
+                  </div>`;
 
-            section.innerHTML = popup;
+                  section.innerHTML = popup;
+                  popupContainer.appendChild(section)
+
+}
+
+const close = () => {
+
+  const closeBtn = document.querySelector('.close');
+
+  closeBtn.addEventListener('click', () => {
+    popupContainer.classList.toggle('hide');
+  });
   
 }
 
-const btn = document.querySelector('button');
+const btn = document.querySelector('.test-btn');
 
 btn.addEventListener('click', getJSONData);
 
-getJSONData();
+// getJSONData();
 
  
