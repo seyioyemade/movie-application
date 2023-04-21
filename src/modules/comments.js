3548e49177f6b6908be48578b99d605783f69ba9
Seyi Oyemade <seyioyemade918@gmail.com>
2023-04-22 00:29:14 +0100

    Add comments counter

--- src/modules/comments.js
+++ src/modules/comments.js
@@ -1,10 +1,14 @@
-export const commentsData = async (ul, id) => {
+import commentsCounter from './counter-comments.js';
+
+export const commentsData = async (id, ul, header) => {
   const response = await fetch(`https://us-central1-involvement-api.cloudfunctions.net/capstoneApi/apps/l9F9LbmS8FpilLXhSiV3/comments?item_id=item${id}`);
   const jsonData = await response.json();
 
-  jsonData.forEach((item) => {
+  await jsonData.forEach((item) => {
     ul.innerHTML += `<li>${item.creation_date} ${item.username}: ${item.comment}</li>`;
   });
+
+  commentsCounter(ul, header);
 };
 
 export const postData = async (id, input, textarea) => {
@@ -30,13 +34,14 @@ export const updateData = async (id, ul) => {
   ul.innerHTML += `<li>${lastItem.creation_date} ${lastItem.username}: ${lastItem.comment}</li>`;
 };
 
-export const addComment = (form, id, input, textarea, ul) => {
+export const addComment = (form, id, input, textarea, header, ul) => {
   form.addEventListener('submit', async (e) => {
     e.preventDefault();
 
     if ((input.value.length > 0) && (textarea.value.length > 0)) {
       await postData(id, input, textarea);
       await updateData(id, ul);
+      await commentsCounter(ul, header);
     }
 
     form.reset();
